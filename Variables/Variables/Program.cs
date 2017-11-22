a45f958d3215595985ee0aa8c63ad2331aa47786
Bea Ortiz <ortizwebdevelopment@gmail.com>
2017-11-21 16:24:22 -0800

    Intro to arrays. Added comments, including the program output.

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using CSharpFundamentals.Math;
 
 
 namespace CSharpFundamentals
@@ -8,14 +7,29 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            Person john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
-            john.Introduce();
+            var numbers = new int[3];
+            numbers[0] = 1;
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]); // defaults to 0 if not initialized
+            Console.WriteLine(numbers[2]); // defaults to 0 if not initialized
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]); // defaults to False if not initialized
+            Console.WriteLine(flags[2]); // defaults to False if not initialized
+
+            var names = new string[3] { "Jack", "John", "Mary" };
         }
     }
 }
+/*Output:
+1
+0
+0
+True
+False
+False
+*/
