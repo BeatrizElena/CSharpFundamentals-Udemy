d5fd3da6b5a8a7d5da4aad0d522594579470c549
Bea Ortiz <ortizwebdevelopment@gmail.com>
2017-11-27 11:20:22 -0800

    strings, string.Format, string.Join, verbatim strings

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -7,29 +7,35 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var numbers = new int[3];
-            numbers[0] = 1;
+            string firstName = "Bea";
+            String lastName = "Ortiz";
+            string myName = "Bea"; // string & String are the same but String needs System namespace on top.
+            // concatenation ex.
+            var fullName = firstName + " " + lastName;
+            // string.Format ex.
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+            //string.Join ex.
+            var names = new string[3] { "John", "Jack", "Mary" };
+            // combine with the string.Join() method where the 1st argument is a string
+            // which represents a separator (using the comma as the separator)
+            //and the 2nd argument is the name of the array.
+            var formattedNames = string.Join(",", names);
+            //display the results on the console
+            Console.WriteLine(formattedNames);
+            // using verbatim strings
+            // messy look of normal string
+            var text = "Hi John\nLook into the following paths\nc:\\folder1\\folder2\nc:\\folder3\\folder4";
+            Console.WriteLine(text);
+            //cleaned up look of verbatim string
+            var text2 = @"Hi John
+Look into the following paths
+c:\folder1\folder2
+c:\folder3\folder4";
+            Console.WriteLine(text2);
 
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]); // defaults to 0 if not initialized
-            Console.WriteLine(numbers[2]); // defaults to 0 if not initialized
-
-            var flags = new bool[3];
-            flags[0] = true;
-
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]); // defaults to False if not initialized
-            Console.WriteLine(flags[2]); // defaults to False if not initialized
-
-            var names = new string[3] { "Jack", "John", "Mary" };
         }
     }
 }
 /*Output:
-1
-0
-0
-True
-False
-False
+
 */
