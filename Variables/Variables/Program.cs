6ff0146392d9ba340939816ae5034f00d3fc5301
Bea Ortiz <ortizwebdevelopment@gmail.com>
2017-11-28 15:44:35 -0800

    Value Types & Reference Types (IV-36)

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -5,34 +5,29 @@ namespace CSharpFundamentals
 {
     class Program
     {
-        public enum ShippingMethod
-        {
-            RegularAirMail = 1,
-            RegisteredAirMail = 2,
-            Express = 3
-        }
         static void Main(string[] args)
         {
-            var method = ShippingMethod.Express;
-            Console.WriteLine((int)method);
+            // Value types
+            var a = 10; // value 10 is saved in a specific stack location
+            // Copy a to b. A new location in stack is created to hold the value of b.
+            var b = a; 
+            // increment b by one, a does not change bc its value was copied
+            b++;
+            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
 
-            //convert a given code to a corresponding shipping method
-            var methodId = 3;
-            Console.WriteLine((ShippingMethod)methodId);
-            Console.WriteLine(method.ToString()); //.ToString() is not absolutely needed in this line
-
-            // Parse a string into a shipping method. Or, converting from method name to shipping method enum: 
-            // Using parsing (i.e. getting a string and converting it to a different type.)
-            // Use .NET class Enum, defined in the System namespace, which has several methods, including 
-            // .Parse(parameter1: Type enum Type, parameter2: string value that returns an object, not a shipping method)
-            // to get the shipping method, we need to parse the object as (ShippingMethod) at the beginning of the line.
-            // finally, we can store the value of the expression in a var, e.g. shippingMethod
-            var methodName = "Express";
-            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
-            Console.WriteLine(shippingMethod);
+            // Reference types
+            var array1 = new int[3] { 1, 2, 3 };
+            // copy array1 to array 2
+            var array2 = array1;
+            // Modify array2. This will modify array1 bc both arrays are a REFERENCE to the memory address
+            // The memory address is located in the heap.
+            // Neither var, array1 nor array2, is referencing the actual values
+            array2[0] = 0;
+            Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
         }
     }
 }
 /*Output:
-
+a: 10, b: 11
+array1[0]: 0, array2[0]: 0
 */
