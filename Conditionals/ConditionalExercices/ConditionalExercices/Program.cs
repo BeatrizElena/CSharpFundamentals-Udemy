16227095aa35d7bc3179e144a713144a06d19ab4
Bea Ortiz <ortizwebdevelopment@gmail.com>
2017-12-01 15:42:36 -0800

    Ex2: User types two numbers. Program outputs max of the 2 numbers.

--- Conditionals/ConditionalExercices/ConditionalExercices/Program.cs
+++ Conditionals/ConditionalExercices/ConditionalExercices/Program.cs
@@ -11,6 +11,13 @@ namespace ConditionalExercices
             var number = Convert.ToInt32(Console.ReadLine());
             var intake = (number >= 1 && number <= 10) ? "Valid" : "Invalid";
             Console.WriteLine(intake);
+
+            Console.WriteLine("Enter a number");
+            var number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number");
+            var number2 = Convert.ToInt32(Console.ReadLine());
+            var max = (number1 > number2) ? number1 : number2;
+            Console.WriteLine("Max number is: {0}", max);
         }
     }
 }
