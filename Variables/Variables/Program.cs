29defda2504d99f5e35d79311640ed1e963a286d
Bea Ortiz <ortizwebdevelopment@gmail.com>
2017-12-03 09:47:41 -0800

    Array Methods (VI-52)

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -3,33 +3,79 @@
 
 namespace CSharpFundamentals
 {
-    public class Person2
-    {
-        public int Age;
-    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var number = 1;
-            // pass Increment() method def'd below
-            Increment(number); // value of number is still 1 bc it is a value type and the var number 
-            // is only def'd within the method Main
-            Console.WriteLine(number);
+            var numbers = new[] {3, 7, 9, 2, 14, 6};
+            // Length (non-static method of the instance numbers)
+            Console.WriteLine("Length: " + numbers.Length);
 
-            var person = new Person2() {Age = 20};
-            MakeOld(person);
-            Console.WriteLine(person.Age);
-        }
+            // The follwng are static methods of Array
+            //IndexOf()
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: " + index);
 
-        public static void Increment(int number)
-        {
-            number += 10;
-        }
+            // Clear()
+            Array.Clear(numbers, 0, 2);
+            Console.WriteLine("Effect of Clear()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+            /* outputs:
+             Effect of Clear()
+             0
+             0
+             9
+             2
+             14
+             6
+            */
 
-        public static void MakeOld(Person2 person)
-        {
-            person.Age += 10;
+            // Copy()
+            // declare another array to copy into
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+            Console.WriteLine("Effect of Copy()");
+            foreach (var n in another)
+                Console.WriteLine(n);
+         
+            /* outputs:
+             Effect of Copy()
+             0
+             0
+             9
+            */
+
+            // Sort()
+            Array.Sort(numbers);
+            Console.WriteLine("Effect of Sort()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+            /* outputs:
+            Effect of Sort()
+            0
+            0
+            2
+            6
+            9
+            14
+            */
+
+            // Reverse()
+            Array.Reverse(numbers);
+            Console.WriteLine("Effect of Reverse()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+            /* outputs:
+            Effect of Reverse()
+            14
+            9
+            6
+            2
+            0
+            0
+            */
         }
     }
 }
