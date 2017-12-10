70203c6ba1a7ae885b4070ed38efb1944050bd66
Bea Ortiz <ortizwebdevelopment@gmail.com>
2017-12-10 14:50:01 -0500

    Dates: DateTime (VII-58)

---
+++ CSharpDateTime/CSharpDateTime/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharpDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new System.DateTime(2017, 1, 1);
+            var now = System.DateTime.Now;
+            var today = System.DateTime.Today;
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+
+            // DateTime objects are inmutable. To add/subtract days, use these methods:
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            // Convert to string methods
+            Console.WriteLine("\r\nToLongDateString() method: " + now.ToLongDateString());
+            Console.WriteLine("\nToShortDateString() method: " + now.ToShortDateString());
+            Console.WriteLine("\nToLongTimeString() method: " + now.ToLongTimeString());
+            Console.WriteLine("\nToShortTimeString() method: " + now.ToShortTimeString());
+            Console.WriteLine("\nToString() method displays Date & Time: " + now.ToString());
+            Console.WriteLine("\nToString() with format-specifier: " + now.ToString("yyyy-MM-dd"));
+            Console.WriteLine("\nToString() with more format-specifiers: " + now.ToString("yyyy-MM-dd HH:mm"));
+        }
+    }
+}
---
+++ ExercisesSessionVI/CSharp1ExercisesVI.ArraysAndLists/CSharp1ExercisesVI.ArraysAndLists/Lists.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp1ExercisesVI.ArraysAndLists
+{
+    public class Lists
+    {
+        public void Exercise1()
+        {
+            /* Continuously ask the user to enter different names, until the user presses Enter (without supplying a name). 
+                Depending on the number of names provided, display a message.*/
+            var names = new List<string>();
+            Console.WriteLine("Enter as many names as you'd like. Press Enter after each name. When done, just press Enter");
+            while (true)
+            {
+                var name = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(name))
+                break;
+                names.Add(name);
+            }
+
+            if (names.Count == 1)
+            Console.WriteLine(names[0] + " likes your post");
+            else if (names.Count == 2)
+            Console.WriteLine(names[0] + " and " + names[1] + " like your post");
+            else
+            {
+                var others = names.Count - 2;
+                Console.WriteLine(names[0] + ", " + names[1] + ", and " + others + " others like your post");
+            }
+        }
+
+        //public void Exercise2()
+        //{
+        //    /* Ask the user to enter their name.  Use an array to reverse the name and then store the result in a new string. 
+        //    Display the reversed name on the console. */
+        //    Console.WriteLine("Enter your name");
+        //    var userName = Console.ReadLine();
+        //    var nameArray = new char[userName.Length];
+        //    Console.WriteLine(nameArray);
+        //}
+    }
+}
