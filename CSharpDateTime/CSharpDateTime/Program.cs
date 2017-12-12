58f60326129561cc75d562e1558c3ced169c0ffa
Bea Ortiz <ortizwebdevelopment@gmail.com>
2017-12-12 18:25:26 -0500

    Strings: Trim, ToUpper, IndexOf, Substring, Split, IsNullOrWhiteSpace, Convert.ToByte (VIII-63)

--- CSharpDateTime/CSharpDateTime/Program.cs
+++ CSharpDateTime/CSharpDateTime/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-// TimeSpan()
+
 namespace CSharpDateTime
 {
     class Program
@@ -7,7 +7,33 @@ namespace CSharpDateTime
         static void Main(string[] args)
         {
             var fullName = "Bea Ortiz ";
-            Console.WriteLine("Trim(): '{0}", fullName.Trim());
+            Console.WriteLine("Trim(): '{0}'", fullName.Trim());
+            Console.WriteLine("Trim().ToUpper: '{0}'", fullName.Trim().ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("First Name: " + firstName);
+            Console.WriteLine("Last Name: " + lastName);
+
+            var name = fullName.Split(' ');
+            Console.WriteLine("First Name: " + name[0]);
+            Console.WriteLine("Last Name: " + name[1]);
+
+            Console.WriteLine(fullName.Replace("Bea", "Beatriz"));
+
+            if (String.IsNullOrWhiteSpace(" "))
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            var str = "25";
+            var age = Convert.ToByte((str));
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C"));
+            Console.WriteLine(price.ToString("C0"));
         }
     }
 }
