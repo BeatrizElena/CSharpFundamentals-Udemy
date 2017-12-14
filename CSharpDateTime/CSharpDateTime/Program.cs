1c34594030e16261db34045b0541b9b6819b96ea
Bea Ortiz <ortizwebdevelopment@gmail.com>
2017-12-14 11:01:43 -0500

    StringBuilder Methods (VII-66)

--- CSharpDateTime/CSharpDateTime/Program.cs
+++ CSharpDateTime/CSharpDateTime/Program.cs
@@ -1,17 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Text;
 
 
-// Add new class to project
 namespace CSharpDateTime
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var sentence = "This is going to be a really really really really really really long sentence.";
-            var summary = StringUtility.SummarizeText(sentence);
-            Console.WriteLine(summary);
+            var builder = new StringBuilder("Hello World");
+
+            builder.Append('-', 10);
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-', 10);
+            Console.WriteLine(builder);
+
+            builder.Replace('-', '+');
+            Console.WriteLine("Replace() - with +: " + builder);
+
+            builder.Remove(0, 10);
+            Console.WriteLine("Remove()  10 chars: " + builder);
+
+            builder.Insert(0, new string('$', 10));
+            Console.WriteLine("Insert(): " + builder);
+
+            Console.WriteLine("First Char (builder[0]: " + builder[0]);
         }
 
     }
