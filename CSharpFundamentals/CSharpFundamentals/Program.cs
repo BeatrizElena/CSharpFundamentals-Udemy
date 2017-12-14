42493561307c17ff6dcab13469f4ebff28288074
Bea Ortiz <ortizwebdevelopment@gmail.com>
2017-12-14 12:50:01 -0500

    Procedural Prog. Ex 1-Separate logic from console code (VII-68)

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -9,18 +9,25 @@ namespace CSharpFundamentals
 {
     internal class Program
     {
+        // Main class will have only the code that works with the console.
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter your name: ");
             var name = Console.ReadLine();
+            // call the method ReverseName
+            var reversed = ReverseName(name);
+            Console.WriteLine("Reversed name: " + reversed);
+        }
 
+        // public string class into which to put the code that deals with logic.
+        public static string ReverseName(string name)
+        {
             var array = new char[name.Length];
             for (var i = name.Length; i > 0; i--)
                 array[name.Length - i] = name[i - 1];
 
             var reversed = new string(array);
-
-            Console.WriteLine("Reversed name: " + reversed);
+            return reversed;
         }
     }
 }
