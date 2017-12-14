a979ca49282b69ea5e7512d1ac459569d43229a5
Bea Ortiz <ortizwebdevelopment@gmail.com>
2017-12-14 11:04:34 -0500

    StringBuilder-Chaining the Methods (VIII-66)

--- CSharpDateTime/CSharpDateTime/Program.cs
+++ CSharpDateTime/CSharpDateTime/Program.cs
@@ -10,20 +10,15 @@ namespace CSharpDateTime
         {
             var builder = new StringBuilder("Hello World");
 
-            builder.Append('-', 10);
-            builder.AppendLine();
-            builder.Append("Header");
-            builder.AppendLine();
-            builder.Append('-', 10);
-            Console.WriteLine(builder);
-
-            builder.Replace('-', '+');
-            Console.WriteLine("Replace() - with +: " + builder);
-
-            builder.Remove(0, 10);
-            Console.WriteLine("Remove()  10 chars: " + builder);
-
-            builder.Insert(0, new string('$', 10));
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('$', 10));
             Console.WriteLine("Insert(): " + builder);
 
             Console.WriteLine("First Char (builder[0]: " + builder[0]);
