912e9845bdf9ec4de7150b41532e91e291b2b526
Bea Ortiz <ortizwebdevelopment@gmail.com>
2017-12-14 10:13:25 -0500

    Summarizing Text-improving the code

--- CSharpDateTime/CSharpDateTime/Program.cs
+++ CSharpDateTime/CSharpDateTime/Program.cs
@@ -8,30 +8,33 @@ namespace CSharpDateTime
         static void Main(string[] args)
         {
             var sentence = "This is going to be a really really really really really really long sentence.";
-            const int maxLength = 20;
+            var summary = SummarizeText(sentence);
+            Console.WriteLine(summary);
+        }
 
-            if (sentence.Length < maxLength)
+        static string SummarizeText(string text, int maxLength = 25)
+        {
+            if (text.Length < maxLength)
             {
-                Console.WriteLine(sentence);
+                return text;
             }
-            else
+
+            var words = text.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+
+            foreach (var word in words)
             {
-                var words = sentence.Split(' ');
-                var totalCharacters = 0;
-                var summaryWords = new List<string>();
+                summaryWords.Add(word);
 
-                foreach (var word in words)
-                {
-                    summaryWords.Add(word);
-
-                    totalCharacters += word.Length + 1;
-                    if (totalCharacters > maxLength)
-                        break;
-                }
-
-                var summary = String.Join(" ", summaryWords) + "...";
-                Console.WriteLine(summary);
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength)
+                    break;
             }
+
+            //var summary = String.Join(" ", summaryWords) + "...";
+            //return summary;
+            return String.Join(" ", summaryWords) + "...";
         }
     }
 }
